56eca5048ceb99799b78b80167658074d4ce7a96
Max Sharing <revcheg0@gmail.com>
2024-06-17 16:47:43 +0300

    Разбил файлы на более логичную структуру, другой вывод данных

--- js/card.js
+++ js/card.js
@@ -1,4 +1,5 @@
 import {arrayCards} from './data.js';
+import {setContent, setFeatures, setPhotos, setAvatar} from './utilities.js';
 
 const mapElement = document.querySelector('.map__canvas');
 const cardTepmlate = document.querySelector('#card').content.querySelector('.popup');
@@ -8,62 +9,17 @@ const similarCardFragment = document.createDocumentFragment();
 arrayCards.forEach((card) => {
   const cardElement = cardTepmlate.cloneNode(true);
 
-  const setTextContent = (selector, content) => {
-    const element = cardElement.querySelector(selector);
-    if (content) {
-      element.textContent = content;
-    } else {
-      element.classList.add('hidden');
-    }
-  };
+  setContent(cardElement, '.popup__title', card.offer.title);
+  setContent(cardElement, '.popup__text--address', card.offer.address);
+  setContent(cardElement, '.popup__text--price', `${card.offer.price} ₽/ночь`);
+  setContent(cardElement, '.popup__type', card.offer.type);
+  setContent(cardElement, '.popup__text--capacity', `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`);
+  setContent(cardElement, '.popup__text--time', `Заезд после ${card.offer.checkin}, выезд до ${card.offer.checkout}`);
+  setContent(cardElement, '.popup__description', card.offer.description);
 
-  setTextContent('.popup__title', card.offer.title);
-  setTextContent('.popup__text--address', card.offer.address);
-  setTextContent('.popup__text--price', `${card.offer.price} ₽/ночь`);
-  setTextContent('.popup__type', card.offer.type);
-  setTextContent('.popup__text--capacity', `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`);
-  setTextContent('.popup__text--time', `Заезд после ${card.offer.checkin}, выезд до ${card.offer.checkout}`);
-  setTextContent('.popup__description', card.offer.description);
-
-  const featureList = cardElement.querySelector('.popup__features');
-
-  if (card.offer.features.length === 0) {
-    featureList.classList.add('hidden');
-  }
-
-  featureList.innerHTML = '';
-
-  card.offer.features.forEach((feature) => {
-    let featureElement = document.createElement('li');
-    featureElement.classList.add('popup__feature', `popup__feature--${feature}`);
-    featureList.appendChild(featureElement);
-  });
-
-  const photoList = cardElement.querySelector('.popup__photos');
-
-  if (card.offer.photos.length === 0) {
-    photoList.classList.add('hidden');
-  }
-
-  photoList.innerHTML = '';
-
-  card.offer.photos.forEach((photo) => {
-    let photoElement = document.createElement('img');
-    photoElement.src = photo;
-    photoElement.classList.add('popup__photo');
-    photoElement.width = 45;
-    photoElement.height = 40;
-    photoElement.alt = 'Фотография жилья';
-    photoList.appendChild(photoElement);
-  });
-
-  let avatarElement = cardElement.querySelector('.popup__avatar');
-
-  if (card.author.avatar) {
-    avatarElement.src = card.author.avatar;
-  } else {
-    avatarElement.classList.add('hidden');
-  }
+  setFeatures(cardElement, card.offer.features);
+  setPhotos(cardElement, card.offer.photos);
+  setAvatar(cardElement, card.author.avatar);
 
   similarCardFragment.appendChild(cardElement);
 });
