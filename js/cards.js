792b2815d8ca118dc0d4919478e1fcc3654ce48f
Max Sharing <revcheg0@gmail.com>
2024-06-18 17:46:14 +0300

    Инициализация пинов на карте и вывод попапов объявлений

--- js/cards.js
+++ js/cards.js
@@ -1,27 +1,27 @@
-import {cards} from './data.js';
-import {setContent, setFeatures, setPhotos, setAvatar} from './utilities.js';
+// import {cards} from './data.js';
+// import {setContent, setFeatures, setPhotos, setAvatar} from './utilities.js';
 
-const mapElement = document.querySelector('.map__canvas');
-const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+// const mapElement = document.querySelector('.map__canvas');
+// const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-const cardsFragment = document.createDocumentFragment();
+// const cardsFragment = document.createDocumentFragment();
 
-cards.forEach(({offer, author}) => {
-  const cardElement = cardTemplate.cloneNode(true);
+// cards.forEach(({offer, author}) => {
+//   const cardElement = cardTemplate.cloneNode(true);
 
-  setContent(cardElement, '.popup__title', offer.title);
-  setContent(cardElement, '.popup__text--address', offer.address);
-  setContent(cardElement, '.popup__text--price', `${offer.price} ₽/ночь`);
-  setContent(cardElement, '.popup__type', offer.type);
-  setContent(cardElement, '.popup__text--capacity', `${offer.rooms} комнаты для ${offer.guests} гостей`);
-  setContent(cardElement, '.popup__text--time', `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
-  setContent(cardElement, '.popup__description', offer.description);
+//   setContent(cardElement, '.popup__title', offer.title);
+//   setContent(cardElement, '.popup__text--address', offer.address);
+//   setContent(cardElement, '.popup__text--price', `${offer.price} ₽/ночь`);
+//   setContent(cardElement, '.popup__type', offer.type);
+//   setContent(cardElement, '.popup__text--capacity', `${offer.rooms} комнаты для ${offer.guests} гостей`);
+//   setContent(cardElement, '.popup__text--time', `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+//   setContent(cardElement, '.popup__description', offer.description);
 
-  setFeatures(cardElement, offer.features);
-  setPhotos(cardElement, offer.photos);
-  setAvatar(cardElement, author.avatar);
+//   setFeatures(cardElement, offer.features);
+//   setPhotos(cardElement, offer.photos);
+//   setAvatar(cardElement, author.avatar);
 
-  cardsFragment.appendChild(cardElement);
-});
+//   cardsFragment.appendChild(cardElement);
+// });
 
-mapElement.appendChild(cardsFragment);
+// mapElement.appendChild(cardsFragment);
--- js/map.js
+++ js/map.js
@@ -1,17 +1,21 @@
 // import L from 'leaflet';
+import {cards} from './data.js';
+import {setContent, setFeatures, setPhotos, setAvatar} from './utilities.js';
 
-const STARTING_LATITUDE = 35.6804;
-const STARTING_LONGITUDE = 139.7690;
-const STARTING_ZOOM = 9;
+const START_LATITUDE = 35.6804;
+const START_LONGITUDE = 139.7690;
+const START_ZOOM = 12;
+const MAIN_PIN_WIDTH = 52;
+const PIN_WIDTH = 40;
 
 const map = L.map('map-canvas');
 
 const setupMap = () => {
   map
     .setView({
-      lat: STARTING_LATITUDE,
-      lng: STARTING_LONGITUDE,
-    }, STARTING_ZOOM);
+      lat: START_LATITUDE,
+      lng: START_LONGITUDE,
+    }, START_ZOOM);
 
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -22,3 +26,76 @@ const setupMap = () => {
 };
 
 setupMap();
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/main-pin.svg',
+  iconSize: [MAIN_PIN_WIDTH, MAIN_PIN_WIDTH],
+  iconAnchor: [MAIN_PIN_WIDTH / 2, MAIN_PIN_WIDTH],
+});
+
+const mainPin = L.marker(
+  {
+    lat: START_LATITUDE,
+    lng: START_LONGITUDE
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon
+  }
+);
+
+mainPin.addTo(map);
+
+// mainMarker.on('moveend', (evt) => {
+//   console.log(evt.target.getLatLng());
+// });
+
+const createPopup = (offer, author) => {
+  const popupTemplate = document.querySelector('#card').content.querySelector('.popup');
+  const popupElement = popupTemplate.cloneNode(true);
+
+  setContent(popupElement, '.popup__title', offer.title);
+  setContent(popupElement, '.popup__text--address', offer.address);
+  setContent(popupElement, '.popup__text--price', `${offer.price} ₽/ночь`);
+  setContent(popupElement, '.popup__type', offer.type);
+  setContent(popupElement, '.popup__text--capacity', `${offer.rooms} комнаты для ${offer.guests} гостей`);
+  setContent(popupElement, '.popup__text--time', `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+  setContent(popupElement, '.popup__description', offer.description);
+
+  setFeatures(popupElement, offer.features);
+  setPhotos(popupElement, offer.photos);
+  setAvatar(popupElement, author.avatar);
+
+  return popupElement;
+};
+
+const setupPins = () => {
+  const pinIcon = L.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [PIN_WIDTH, PIN_WIDTH],
+    iconAnchor: [PIN_WIDTH / 2, PIN_WIDTH],
+  });
+
+  cards.forEach(({location, offer, author}) => {
+    const pin = L.marker(
+      {
+        lat: location.x,
+        lng: location.y
+      },
+      {
+        icon: pinIcon
+      }
+    );
+
+    pin
+      .addTo(map)
+      .bindPopup(
+        createPopup(offer, author),
+        {
+          keepInView: true
+        }
+      );
+  });
+};
+
+setupPins();
